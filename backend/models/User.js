9aa7404b9bc121c02c51c6b547cce22e56d66ce6
Pmack0702 <meetpatel7026@gmail.com>
2025-02-05 13:56:34 -0800

    Html Pages Created

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,4 +8,14 @@ const UserSchema = new mongoose.Schema({
     createdOn: { type: Date, default: Date.now }
 });
 
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
+  
+UserSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
 module.exports = mongoose.model('users', UserSchema);
